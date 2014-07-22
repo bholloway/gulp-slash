d09142eb1db35e13d1c5b7c41a66e712673445b6
benholloway <ben.holloway@3plearning.com>
2014-07-22 12:06:38 +1000

    fixed module export

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var through = require('through2');
  * @param {string} [text] Text to convert (per node slash)
  * @returns {string|Transform} Converted text where given, else a transform stream for gulp
  */
-exports.slash = function(text) {
+module.exports = function(text) {
   'use strict';
   if (arguments.length > 0) {
     return slash(text);
