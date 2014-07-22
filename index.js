2fbecc1e6bf95475d229b1ead4b00bb751a34d98
benholloway <ben.holloway@3plearning.com>
2014-07-22 11:32:01 +1000

    initial author

---
+++ index.js
@@ -0,0 +1,25 @@
+var slash = require('slash');
+var through = require('through2')
+
+/**
+ * Convert given text using node slash, or where no text is given, return a stream
+ * that similarly converts gulp (vinyl) file paths in place.
+ * @param {string} [text] Text to convert (per node slash)
+ * @returns {string|Transform}
+ */
+exports.slash = function slash(text) {
+  if (arguments.length > 0) {
+    return slash(text);
+  } else {
+    return through.obj(function(file, encoding, done) {
+      [ 'path', 'cwd', 'base' ].forEach(function(field) {
+        var isValid = (field in file) && (typeof file[field] == typeof '');
+        if (isValid) {
+          file[field] = slash(file[field]);
+        }
+      });
+      this.push(file);
+      done();
+    });
+  }
+}
