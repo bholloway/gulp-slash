5db32c510b39223c3134d4012d77832d3cb43a26
benholloway <ben.holloway@3plearning.com>
2014-07-23 10:29:50 +1000

    added direct conversion of vinyl files where given

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ var through = require('through2');
 /**
  * Convert given text using node slash, or where no text is given, return a stream
  * that similarly converts gulp (vinyl) file paths in place.
- * @param {string} [text] Text to convert (per node slash)
- * @returns {string|Transform} Converted text where given, else a transform stream for gulp
+ * @param {string|File} [candidate] Text or a vinyl file to convert
+ * @returns {string|Transform} Converted text or file where given, else a transform stream for gulp
  */
-module.exports = function(text) {
+function gulpSlash(candidate) {
   'use strict';
-  if (arguments.length > 0) {
-    return !!(text) ? slash(text) : text;
-  } else {
+
+  // no arguments implies stream
+  if (arguments.length === 0) {
     return through.obj(function(file, encoding, done) {
-      [ 'path', 'cwd', 'base' ].forEach(function(field) {
-        var isValid = (field in file) && (typeof file[field] === typeof '');
-        if (isValid) {
-          file[field] = slash(file[field]);
-        }
-      });
-      this.push(file);
+      this.push(gulpSlash(file));
       done();
     });
+
+  // text implies direct conversion
+  } else if (typeof candidate === typeof '') {
+    return slash(candidate);
+
+  // object implies path,cwd,base fields are converted
+  } else if (typeof candidate === typeof { }) {
+    [ 'path', 'cwd', 'base' ].forEach(function (field) {
+      var isValid = (field in candidate) && (typeof candidate[field] === typeof '');
+      if (isValid) {
+        candidate[field] = slash(candidate[field]);
+      }
+    });
+    return candidate;
   }
-};
+}
+module.exports = gulpSlash;
